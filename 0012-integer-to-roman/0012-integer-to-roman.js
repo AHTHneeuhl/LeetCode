267925054ca6ad888b1723f6553347c34802b0de
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2022-10-20 11:16:25 +0530

    Time: 229 ms (43.80%), Space: 46.6 MB (92.12%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const val = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+const rom = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"];
+
+var intToRoman = function(num) {
+    let answer = "";
+    for (let i = 0; num; i++)
+        while (num >= val[i]) answer += rom[i], num -= val[i];
+    return answer;
+};
