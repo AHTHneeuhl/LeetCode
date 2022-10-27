505b151c9017ea2160c92ecf8dbc2fecd935899d
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2022-10-27 15:55:21 +0530

    Time: 900 ms (62.57%), Space: 14.6 MB (43.86%) - LeetHub

---
+++ 0835-image-overlap/0835-image-overlap.py
@@ -0,0 +1,25 @@
+class Solution:
+    def largestOverlap(self, img1: List[List[int]], img2: List[List[int]]) -> int:
+        dim = len(img1)
+
+        def non_zero_cells(M):
+            ret = []
+            for x in range(dim):
+                for y in range(dim):
+                    if M[x][y] == 1:
+                        ret.append((x, y))
+            return ret
+
+        transformation_count = defaultdict(int)
+        max_overlaps = 0
+
+        A_ones = non_zero_cells(img1)
+        B_ones = non_zero_cells(img2)
+
+        for (x_a, y_a) in A_ones:
+            for (x_b, y_b) in B_ones:
+                vec = (x_b - x_a, y_b - y_a)
+                transformation_count[vec] += 1
+                max_overlaps = max(max_overlaps, transformation_count[vec])
+
+        return max_overlaps
