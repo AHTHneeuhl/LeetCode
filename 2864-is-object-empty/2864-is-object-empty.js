6e656c65cb81f8a29459a1da65b7b5fb76b25c2b
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-01 13:36:49 +0530

    Time: 39 ms (99.86%), Space: 42.6 MB (52.05%) - LeetHub

--- 2864-is-object-empty/2864-is-object-empty.js
+++ 2864-is-object-empty/2864-is-object-empty.js
@@ -3,9 +3,6 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    if (Array.isArray(obj)) {
-        return obj.length === 0;
-    } else if (typeof obj === 'object') {
-        return Object.keys(obj).length === 0;
-    }
+    for (const _ in obj) return false;
+    return true;
 };
