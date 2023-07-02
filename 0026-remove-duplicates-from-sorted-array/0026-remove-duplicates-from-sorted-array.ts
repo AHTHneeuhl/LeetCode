f9c42dcfd17713fa45ab3838af430b2529e7cbf0
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-02 07:32:50 +0530

    Time: 94 ms (29.81%), Space: 45.7 MB (49.98%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,14 +1,13 @@
 function removeDuplicates(nums: number[]): number {
-    let slow = 1;
-    let fast = 1;
+    if (!nums.length) return 0;
+    let i = 0;
 
-    while (fast < nums.length) {
-        if (nums[slow - 1] !== nums[fast]) {
-            nums[slow] = nums[fast];
-            slow += 1;
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[j] !== nums[i]) {
+            i += 1;
+            nums[i] = nums[j];
         }
-        fast += 1;
     }
 
-    return slow;
+    return i + 1;
 };
