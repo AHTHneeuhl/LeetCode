117a8865a62b60cb4555f94e8f6e06dc4f9e015f
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-07 10:14:35 +0530

    Time: 432 ms (60.97%), Space: 16.7 MB (85.98%) - LeetHub

--- 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
+++ 2134-maximize-the-confusion-of-an-exam/2134-maximize-the-confusion-of-an-exam.py
@@ -1,25 +1,16 @@
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        n = len(answerKey)
-        left, right = k,  n
+        max_size = k
+        count = Counter(answerKey[:k])
 
-        def isValid(size):
-            counter = Counter(answerKey[:size])
-            if min(counter['T'], counter['F']) <= k:
-                return True
-            for i in range(size, n):
-                counter[answerKey[i]] += 1
-                counter[answerKey[i - size]] -= 1
-                if min(counter['T'], counter['F']) <= k:
-                    return True
-            return False
+        left = 0
+        for right in range(k, len(answerKey)):
+            count[answerKey[right]] += 1
 
-        while left < right:
-            mid = (left + right + 1)  // 2
+            while min(count['F'], count['T']) > k:
+                count[answerKey[left]] -= 1
+                left += 1
 
-            if isValid(mid):
-                left = mid
-            else:
-                right = mid - 1
-        
-        return left
+            max_size = max(max_size, right - left + 1)
+
+        return max_size
