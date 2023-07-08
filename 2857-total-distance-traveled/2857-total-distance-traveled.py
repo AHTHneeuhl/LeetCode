5dbde70ddf47fff5f7794093adb6cfae95bc7ad9
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-09 01:41:35 +0530

    Time: 68 ms (96.56%), Space: 16.4 MB (9.55%) - LeetHub

--- 2857-total-distance-traveled/2857-total-distance-traveled.py
+++ 2857-total-distance-traveled/2857-total-distance-traveled.py
@@ -1,14 +1,5 @@
 class Solution:
     def distanceTraveled(self, mainTank: int, additionalTank: int) -> int:
-        total = 0
-
-        while mainTank >= 5:
-            mainTank = mainTank - 5
-            if additionalTank >= 1:
-                additionalTank = additionalTank - 1
-                mainTank += 1
-            total += 5
-
-        return (total + mainTank) * 10
+        return (mainTank + min((mainTank - 1) // 4, additionalTank)) * 10
 
         
