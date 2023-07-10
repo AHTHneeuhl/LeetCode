04fccaf9fe56b3f54b4b72da37d6b80736462a13
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-10 14:53:44 +0530

    Time: 112 ms (22.48%), Space: 49 MB (36.24%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,24 +1,3 @@
 function isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) return false;
-
-    const charFreq: { [key: string]: number } = {};
-
-    for (const char of s) {
-        charFreq[char] = (charFreq[char] || 0) + 1;
-    }
-
-    for (const char of t) {
-        if (!charFreq[char]) {
-            return false;
-        }
-        charFreq[char]--;
-    }
-
-    for (const key in charFreq) {
-        if (charFreq[key] !== 0) {
-            return false;
-        }
-    }
-
-    return true;
+  return JSON.stringify([...s].sort()) === JSON.stringify([...t].sort());
 };
