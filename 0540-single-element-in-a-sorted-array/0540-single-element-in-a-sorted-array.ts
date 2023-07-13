4136a587abdc330e7163f8a494adf34a1b8c49d5
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-13 22:23:19 +0530

    Time: 68 ms (54.69%), Space: 45.3 MB (81.25%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.ts
@@ -0,0 +1,12 @@
+function singleNonDuplicate(nums: number[]): number {
+    let l = 0, r = nums.length - 2;
+
+    while (l <= r) {
+        const m = (l + r) >> 1;
+
+        if (nums[m] === nums[m ^ 1]) l = m + 1;
+        else r = m - 1;
+    }
+
+    return nums[l];
+};
