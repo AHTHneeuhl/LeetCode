4e917451b6f29aafe1529825fe2fd7d5706b3a49
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-07-19 11:33:18 +0530

    Time: 562 ms (33.33%), Space: 77.5 MB (55.56%) - LeetHub

---
+++ 2306-naming-a-company/2306-naming-a-company.ts
@@ -0,0 +1,38 @@
+function distinctNames(ideas: string[]): number {
+    const wordMap: Map<string, Set<string>> = new Map();
+    let count = 0;
+
+    ideas.forEach((word) => {
+        const ch = word.charAt(0);
+        const substr = word.slice(1);
+        const set = wordMap.get(ch) ?? new Set();
+        set.add(substr);
+        wordMap.set(ch, set);
+    });
+
+    for (const [ch1, set1] of wordMap.entries()) {
+        for (const [ch2, set2] of wordMap.entries()) {
+            if (ch1 === ch2) {
+                continue;
+            }
+
+            const intersect = calculateIntersection(set1, set2);
+
+            const distinct1 = set1.size - intersect;
+            const distinct2 = set2.size - intersect;
+            count += distinct1 * distinct2;
+        }
+    }
+
+    return count;
+}
+
+function calculateIntersection(set1: Set<string>, set2: Set<string>): number {
+    let intersect = 0;
+    set1.forEach((word) => {
+        if (set2.has(word)) {
+            intersect += 1;
+        }
+    });
+    return intersect;
+}
