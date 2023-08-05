11bf5b4526eea72388a82b5dea95a5131ec3d1b4
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-08-05 16:45:34 +0530

    Time: 59 ms (94.03%), Space: 44.9 MB (51.26%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -11,14 +11,25 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    if (list1 == null) return list2;
-    if (list2 == null) return list1;
-    
-    if (list1.val < list2.val){
-        list1.next = mergeTwoLists(list1.next, list2);
-        return list1;
-    } else {
-        list2.next = mergeTwoLists(list1, list2.next);
-        return list2;
+    const head = new ListNode(-1);
+    let curr: ListNode | null = head;
+
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            curr.next = list1;
+            list1 = list1.next;
+        } else {
+            curr.next = list2;
+            list2 = list2.next;
+        }
+        curr = curr.next;
     }
+
+    if (list1) {
+        curr.next = list1;
+    } else if (list2) {
+        curr.next = list2;
+    }
+
+    return head.next;
 };
