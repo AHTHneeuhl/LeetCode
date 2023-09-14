be607c9306fbdb8c0e705368aaffe4f7b7d60f55
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-09-14 16:04:45 +0530

    Time: 57 ms (53.29%), Space: 44 MB (15%) - LeetHub

--- 0198-house-robber/0198-house-robber.ts
+++ 0198-house-robber/0198-house-robber.ts
@@ -1,14 +1,9 @@
 function rob(nums: number[]): number {
-    if (nums.length === 0) return 0;
-    if (nums.length === 1) return nums[0];
+    let prev = 0, curr = 0;
 
-    const dp: number[] = [];
-    dp[0] = nums[0];
-    dp[1] = Math.max(nums[0], nums[1]);
-
-    for (let i = 2; i < nums.length; i++) {
-        dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+    for (let num of nums) {
+        [prev, curr] = [curr, Math.max(prev + num, curr)];
     }
 
-    return dp[nums.length - 1];
+    return curr;
 };
