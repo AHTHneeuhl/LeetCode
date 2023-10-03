3c868afb80e952e8b6e9d58fc98a92651d655141
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-10-03 08:02:06 +0530

    Time: 48 ms (89.69%), Space: 42.6 MB (79.39%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.ts
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.ts
@@ -1,8 +1,8 @@
 function numIdenticalPairs(nums: number[]): number {
     let count = 0;
     for (let i = 0; i < nums.length; i++) {
-        for (let j = 0; j < nums.length; j++) {
-            if (i < j && nums[i] === nums[j]) count += 1;
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] === nums[j]) count += 1;
         }
     }
     return count;
