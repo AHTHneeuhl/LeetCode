4b10a45e2d5000da982444ad9160cd818fd31bab
Shubham Patrick <91504362+AHTHneeuhl@users.noreply.github.com>
2023-10-18 11:53:43 +0530

    Time: 38 ms (99.26%), Space: 18.3 MB (88.91%) - LeetHub

--- 0337-house-robber-iii/0337-house-robber-iii.py
+++ 0337-house-robber-iii/0337-house-robber-iii.py
@@ -7,9 +7,20 @@
 class Solution:
     def rob(self, root: Optional[TreeNode]) -> int:
         def dfs(node):
-            if not node: return [0, 0]
-            L = dfs(node.left)
-            R = dfs(node.right)
-            return [node.val + L[1] + R[1], max(L) + max(R)]
-        
-        return max(dfs(root))
+            if not node:
+                return (0, 0)
+
+            left_robbed, left_not_robbed = dfs(node.left)
+            right_robbed, right_not_robbed = dfs(node.right)
+
+            # If the current node is robbed, its children cannot be robbed
+            robbed = node.val + left_not_robbed + right_not_robbed
+
+            # If the current node is not robbed, we can choose to either rob or not rob its children
+            not_robbed = max(left_robbed, left_not_robbed) + max(right_robbed, right_not_robbed)
+
+            return (robbed, not_robbed)
+
+        robbed, not_robbed = dfs(root)
+        # The result is the maximum of the two possibilities
+        return max(robbed, not_robbed)
